aceca954a1ff4366e26dbff57f7394c82e3205c7
Vargentum <vargentum@gmail.com>
2015-12-28 23:00:01 +0200

    make multiple ajax calls display quotes

--- src/components/quotes/FamousQuotesComponent.js
+++ src/components/quotes/FamousQuotesComponent.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import qwest from 'qwest'
+import update from 'react-addons-update'
+import _ from 'lodash'
 
 require('styles/quotes/FamousQuotes.styl');
 
@@ -17,7 +19,15 @@ class FamousQuotesComponent extends React.Component {
   render() {
     return (
       <div className="famousquotes-component">
-        hello quotes        
+        hello quotes
+        <ul>
+          {this.props.quotes.map(
+            q => <li key={_.uniqueId('quote-')}>
+                    <p>{q.quote}</p>
+                    <p>{q.author} - {q.category}</p>
+                 </li>
+          )}
+        </ul>        
       </div>
     );
   }
@@ -30,36 +40,45 @@ class FamousQuotesComponentContainer extends React.Component {
       this.displayName = 'FamousQuotesComponentContainer';
 
       this.state = {
-        quotes: {}
+        quotes: []
       }
+
+      this.getQuotes = n => {
+        while(n > 0){
+          qwest.get("https://andruxnet-random-famous-quotes.p.mashape.com", null, {
+            headers: {
+              "X-Mashape-Key": "6ZxCnFvSUkmshFVDJrAg9TszqTEJp1OvHeIjsnwqQ4akuwfrB5",
+              "Content-Type": "application/x-www-form-urlencoded",
+              "Accept": "application/json"
+            }
+          })
+          .then((xhr, response) => {
+            this.setState({
+              quotes: update(this.state.quotes, {$push: [JSON.parse(response)]})
+            });
+          })
+          .catch((xhr, response, e) => {
+            console.log(e.name)
+          })
+          .complete((xhr, response) => {
+            console.log(`Complete response ${n}`)
+          }) 
+
+          n-- 
+        }
+      }
+
     }
 
     componentDidMount() {
-      qwest.get("https://andruxnet-random-famous-quotes.p.mashape.com", null, {
-        headers: {
-          "X-Mashape-Key": "6ZxCnFvSUkmshFVDJrAg9TszqTEJp1OvHeIjsnwqQ4akuwfrB5",
-          "Content-Type": "application/x-www-form-urlencoded",
-          "Accept": "application/json"
-        }
-      })
-      .then((xhr, response) => {
-        console.log(response)
-      })
-      .catch((xhr, response, e) => {
-        console.log(e.name)
-      })
-      .complete((xhr, response) => {
-        console.log("Completed!11")
-      })
+      this.getQuotes(10)
     }
 
-/*
-cat={category}
 
-*/
     render() {
+      // this.state.quotes
       return <div>
-               <FamousQuotesComponent quotes={this.state.quotes} />
+               <FamousQuotesComponent {...this.state} />
              </div>
     }
 }
