10284f337e3627b5edbf064f95a9d6421fcd1259
Vargentum <vargentum@gmail.com>
2016-02-01 18:10:48 +0200

    load all quotes at single time point

--- src/components/quotes/FamousQuotesComponent.js
+++ src/components/quotes/FamousQuotesComponent.js
@@ -4,6 +4,7 @@ import React from 'react';
 import qwest from 'qwest'
 import update from 'react-addons-update'
 import _ from 'lodash'
+import Q from 'q'
 
 require('styles/quotes/FamousQuotes.styl');
 
@@ -43,32 +44,50 @@ class FamousQuotesComponentContainer extends React.Component {
         quotes: []
       }
 
-      this.getQuotes = n => {
-        while(n > 0){
-          qwest.get("https://andruxnet-random-famous-quotes.p.mashape.com", null, {
-            headers: {
-              "X-Mashape-Key": "6ZxCnFvSUkmshFVDJrAg9TszqTEJp1OvHeIjsnwqQ4akuwfrB5",
-              "Content-Type": "application/x-www-form-urlencoded",
-              "Accept": "application/json"
-            }
-          })
-          .then((xhr, response) => {
-            this.setState({
-              quotes: update(this.state.quotes, {$push: [JSON.parse(response)]})
-            });
-          })
-          .catch((xhr, response, e) => {
-            console.log(e.name)
-          })
-          .complete((xhr, response) => {
-            console.log(`Complete response ${n}`)
-          }) 
-
-          n-- 
-        }
+      this.getQuote = (resolve, reject) => {
+        qwest.get("https://andruxnet-random-famous-quotes.p.mashape.com", null, {
+          headers: {
+            "X-Mashape-Key": "6ZxCnFvSUkmshFVDJrAg9TszqTEJp1OvHeIjsnwqQ4akuwfrB5",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/json"
+          }
+        }).then((xhr, response) => {
+          resolve(JSON.parse(response))
+        }).then((xhr, error) => {
+          reject(error)
+        })
       }
 
+
+      this.getQuotes = n => {
+
+        let quotes = _.times(n, () => {
+          return Q.promise((resolve, reject) => {
+            this.getQuote(
+              (response) => {
+                resolve(response)
+              },
+              (error) => {
+                reject(error)
+              }
+            )
+          })
+        })
+
+        Q.all(quotes).then(
+          (response) => {
+            this.setState({
+              quotes: response
+            });
+          },
+          (error) => {
+            console.log("No quotes!!!!")
+          })
+      }
     }
+
+
+
 
     componentDidMount() {
       this.getQuotes(10)
